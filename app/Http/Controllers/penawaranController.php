ffcce0cc2c94a139767c92a3666d3e409ebb4909
portgazdbeel <portgaz77@gmail.com>
2023-10-25 03:15:30 +0700

    commit excell

--- app/Http/Controllers/penawaranController.php
+++ app/Http/Controllers/penawaranController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Export\PenawaranExport;
+use App\Import\PemendagriReportExport;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\App;
+use Maatwebsite\Excel\Facades\Excel;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class penawaranController extends Controller
 {
@@ -17,15 +21,15 @@ class penawaranController extends Controller
         $pdf   = App::make('dompdf.wrapper');
         $pdf->loadHTML($this->dataTransaksi($id))->setPaper('A4', 'potrait')->save('Laporan.pdf');
 
-        $data = $this->dataTransaksi($id);
-         return view('admin/project/penawaran', ['data' => $data]);
+//        $data = $this->dataTransaksi($id);
+//         return view('admin/project/penawaran', ['data' => $data]);
         return $pdf->stream();
     }
 
     public function dataTransaksi($id)
     {
         $data  = Project::with(['items.city','items.item'])->findOrFail($id);
-        return $data;
+//        return $data;
         $trans = [];
         $start = \request('start');
         $end   = \request('end');
@@ -34,5 +38,13 @@ class penawaranController extends Controller
         // }
         // $trans = $trans->get();
         return view('admin/project/penawaran', ['data' => $data]);
+    }
+
+    /**
+     * @return BinaryFileResponse
+     */
+    public function exportExcel($id){
+        $data = Project::findOrFail($id);
+        return Excel::download(new PenawaranExport($id, $data->name), $data->name.'.xlsx');
     }
 }
