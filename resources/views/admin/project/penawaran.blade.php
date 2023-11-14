d827e7ea3ac03865314c64fc23c434b793fdfe2d
pradanna <pradanamahendra@gmail.com>
2023-11-14 12:50:19 +0700

    penawaran

--- resources/views/admin/project/penawaran.blade.php
+++ resources/views/admin/project/penawaran.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ $data }}</title>
+    <title>{{ $data->name }}</title>
     <!-- Fonts -->
 
     <!-- Styles -->
